ad3228f9bc3fc178575fc35d97c2b55950462bcb
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-03-29 10:21:16 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,8 +29,8 @@ def won?(board)
     position_3 = board[win_index_3]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      win_combination
+      return win_combination
     end
   end
-  false
+  return false
 end
